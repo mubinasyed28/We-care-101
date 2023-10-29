544e44f05845241ed815cf5f9a6d31d54ab41841
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 07:44:05 +0400

    Add files via upload

---
+++ Add connections/Add Manually/add connection manually.js
@@ -0,0 +1,101 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max7.module.css'
+
+const IPhone1415ProMax7 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max7']}>
+        <img
+          src="/rectangle64638-f7hw-200h.png"
+          alt="Rectangle64638"
+          className={styles['rectangle6']}
+        />
+        <div className={styles['iconparktwotoneback']}>
+          <div className={styles['maskgroup']}>
+            <div className={styles['group']}>
+              <img
+                src="/vectori671-vvo.svg"
+                alt="VectorI671"
+                className={styles['vector']}
+              />
+            </div>
+            <img src="/vectori671-509f.svg" alt="VectorI671" />
+          </div>
+        </div>
+        <span className={styles['text']}>
+          <span>CONNECTIONS</span>
+        </span>
+        <img
+          src="/rectangle616796-nx2u-200h.png"
+          alt="Rectangle616796"
+          className={styles['rectangle61']}
+        />
+        <span className={styles['text02']}>
+          <span>Add</span>
+        </span>
+        <div className={styles['group18']}>
+          <span className={styles['text04']}>
+            <span>ADD MANUALLY</span>
+          </span>
+          <img
+            src="/rectangle374641-cvk4-200h.png"
+            alt="Rectangle374641"
+            className={styles['rectangle37']}
+          />
+          <span className={styles['text06']}>
+            <span>NAME</span>
+          </span>
+          <span className={styles['text08']}>
+            <span>Relation Type</span>
+          </span>
+          <img
+            src="/rectangle384641-n0fa-200w.png"
+            alt="Rectangle384641"
+            className={styles['rectangle38']}
+          />
+          <img
+            src="/rectangle394641-8tsq-200h.png"
+            alt="Rectangle394641"
+            className={styles['rectangle39']}
+          />
+          <span className={styles['text10']}>
+            <span>PHONE NUMBER</span>
+          </span>
+          <span className={styles['text12']}>
+            <span>E.g. Yash Khade</span>
+          </span>
+          <span className={styles['text14']}>
+            <span>9*****</span>
+          </span>
+          <span className={styles['text16']}>
+            <span>+91</span>
+          </span>
+          <div className={styles['group14']}>
+            <img
+              src="/rectangle37i679-qly-200h.png"
+              alt="Rectangle37I679"
+              className={styles['rectangle371']}
+            />
+            <span className={styles['text18']}>
+              <span>Select an Option</span>
+            </span>
+            <img
+              src="/riarrowdropdownlinei679-f8ea.svg"
+              alt="riarrowdropdownlineI679"
+              className={styles['riarrowdropdownline']}
+            />
+          </div>
+          <img
+            src="/vector16799-ztvo.svg"
+            alt="Vector16799"
+            className={styles['vector1']}
+            className={styles['vector1']}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax7
