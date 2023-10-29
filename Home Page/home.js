0bf3ddfbf8efa4d579302aaab5d31bd0bc544315
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 07:35:08 +0400

    Add files via upload

---
+++ Home Page/home.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max3.module.css'
+
+const IPhone1415ProMax3 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max3']}>
+        <img
+          src="/rectangle64215-sbv-200h.png"
+          alt="Rectangle64215"
+          className={styles['rectangle6']}
+        />
+        <span className={styles['text']}>
+          <span>HOME</span>
+        </span>
+        <div className={styles['group3']}>
+          <img
+            src="/rectangle74220-8cgn-200h.png"
+            alt="Rectangle74220"
+            className={styles['rectangle7']}
+          />
+          <img
+            src="/rectangle84223-02dl-200h.png"
+            alt="Rectangle84223"
+            className={styles['rectangle8']}
+          />
+          <img
+            src="/rectangle94224-tm94-200h.png"
+            alt="Rectangle94224"
+            className={styles['rectangle9']}
+          />
+        </div>
+        <div className={styles['group2']}>
+          <img
+            src="/ellipse14225-duxw-400h.png"
+            alt="Ellipse14225"
+            className={styles['ellipse1']}
+          />
+          <img
+            src="/ellipse24226-adp-300h.png"
+            alt="Ellipse24226"
+            className={styles['ellipse2']}
+          />
+          <span className={styles['text2']}>
+            <span>
+              Press Alert Button to Activate SOS and send help message to your
+              connections.
+            </span>
+          </span>
+          <span className={styles['text4']}>
+            <span>ALERT</span>
+          </span>
+          <img
+            src="/rectangle104229-gjj8-200h.png"
+            alt="Rectangle104229"
+            className={styles['rectangle10']}
+          />
+          <span className={styles['text6']}>
+            <span>Emergency Service</span>
+          </span>
+          <img
+            src="/arrow14231-p9pu.svg"
+            alt="Arrow14231"
+            className={styles['arrow1']}
+          />
+        </div>
+        <img
+          src="/materialsymbolscircleoutline6711-k7os.svg"
+          alt="materialsymbolscircleoutline6711"
+          className={styles['materialsymbolscircleoutline']}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax3
