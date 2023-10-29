2577ecdb48f12b9546cd3dceee504ebd327973f7
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 08:03:50 +0400

    Create Set Time.js

---
+++ Custom Tracking/Set Time/Set Time.js
@@ -0,0 +1,192 @@
+import React from 'react'
+
+import styles from './frame2.module.css'
+
+const Frame2 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['frame2']}>
+        <span className={styles['text']}>
+          <span>Select Time</span>
+        </span>
+        <div className={styles['group1']}>
+          <div className={styles['lineshours']}>
+            <img
+              src="/line36074-jxk.svg"
+              alt="Line36074"
+              className={styles['line3']}
+            />
+            <img
+              src="/line36074-zku6.svg"
+              alt="Line36074"
+              className={styles['line31']}
+            />
+          </div>
+          <div className={styles['numbershours']}>
+            <span className={styles['text02']}>
+              <span>12</span>
+            </span>
+            <span className={styles['text04']}>
+              <span>11</span>
+            </span>
+            <span className={styles['text06']}>
+              <span>10</span>
+            </span>
+            <span className={styles['text08']}>9</span>
+            <span className={styles['text09']}>8</span>
+            <span className={styles['text10']}>7</span>
+            <span className={styles['text11']}>6</span>
+            <span className={styles['text12']}>5</span>
+            <span className={styles['text13']}>4</span>
+            <span className={styles['text14']}>3</span>
+            <span className={styles['text15']}>2</span>
+            <span className={styles['text16']}>1</span>
+            <span className={styles['text17']}>
+              <span>00</span>
+            </span>
+            <span className={styles['text19']}>
+              <span>00</span>
+            </span>
+          </div>
+          <img
+            src="/opacityhours6076-g0oq-200w.png"
+            alt="opacityHOURS6076"
+            className={styles['opacity-hours']}
+          />
+        </div>
+        <div className={styles['group2']}>
+          <div className={styles['lines-minutes']}>
+            <img
+              src="/line36076-8ckp.svg"
+              alt="Line36076"
+              className={styles['line32']}
+            />
+            <img
+              src="/line36076-d4tan.svg"
+              alt="Line36076"
+              className={styles['line33']}
+            />
+          </div>
+          <div className={styles['numbers-minutes']}>
+            <span className={styles['text21']}>
+              <span>12</span>
+            </span>
+            <span className={styles['text23']}>
+              <span>11</span>
+            </span>
+            <span className={styles['text25']}>
+              <span>10</span>
+            </span>
+            <span className={styles['text27']}>
+              <span>09</span>
+            </span>
+            <span className={styles['text29']}>
+              <span>08</span>
+            </span>
+            <span className={styles['text31']}>
+              <span>07</span>
+            </span>
+            <span className={styles['text33']}>
+              <span>06</span>
+            </span>
+            <span className={styles['text35']}>
+              <span>05</span>
+            </span>
+            <span className={styles['text37']}>
+              <span>04</span>
+            </span>
+            <span className={styles['text39']}>
+              <span>03</span>
+            </span>
+            <span className={styles['text41']}>
+              <span>02</span>
+            </span>
+            <span className={styles['text43']}>
+              <span>01</span>
+            </span>
+            <span className={styles['text45']}>
+              <span>00</span>
+            </span>
+          </div>
+          <img
+            src="/opacityminutes6078-akdc-200w.png"
+            alt="opacityMINUTES6078"
+            className={styles['opacity-minutes']}
+          />
+        </div>
+        <div className={styles['frame7']}>
+          <img
+            src="/ellipse16079-nikq-200h.png"
+            alt="Ellipse16079"
+            className={styles['ellipse1']}
+          />
+          <img
+            src="/ellipse16079-p76-200h.png"
+            alt="Ellipse16079"
+            className={styles['ellipse11']}
+          />
+        </div>
+        <div className={styles['frame6']}>
+          <img
+            src="/ellipse16076-mmuv-200h.png"
+            alt="Ellipse16076"
+            className={styles['ellipse12']}
+          />
+          <img
+            src="/ellipse16076-0vpl-200h.png"
+            alt="Ellipse16076"
+            className={styles['ellipse13']}
+          />
+        </div>
+        <div className={styles['group3']}>
+          <div className={styles['lineshours1']}>
+            <img
+              src="/line36078-0x3b.svg"
+              alt="Line36078"
+              className={styles['line34']}
+            />
+            <img
+              src="/line36078-u7ha.svg"
+              alt="Line36078"
+              className={styles['line35']}
+            />
+          </div>
+          <div className={styles['ampm']}>
+            <span className={styles['text47']}>
+              <span>PM</span>
+            </span>
+            <span className={styles['text49']}>
+              <span>AM</span>
+            </span>
+            <span className={styles['text51']}>
+              <span>PM</span>
+            </span>
+          </div>
+          <img
+            src="/opacityhours6079-x6sn-200h.png"
+            alt="opacityHOURS6079"
+            className={styles['opacity-hours1']}
+          />
+        </div>
+        <img
+          src="/rectangle596079-0e0y-200h.png"
+          alt="Rectangle596079"
+          className={styles['rectangle59']}
+        />
+        <img
+          src="/rectangle606080-4snt-200h.png"
+          alt="Rectangle606080"
+          className={styles['rectangle60']}
+        />
+        <span className={styles['text53']}>
+          <span>Cancel</span>
+        </span>
+        <span className={styles['text55']}>
+          <span>OK</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Frame2
