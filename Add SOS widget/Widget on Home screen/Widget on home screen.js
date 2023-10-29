d6c12408dd585827ff369be7d7d0f77f8899c4dc
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 08:20:31 +0400

    Create Widget on home screen.js

---
+++ Add SOS widget/Widget on Home screen/Widget on home screen.js
@@ -0,0 +1,113 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max24.module.css'
+
+const IPhone1415ProMax24 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max24']}>
+        <img
+          src="/emojionemonotonewhitecircle6711-ba8.svg"
+          alt="emojionemonotonewhitecircle6711"
+          className={styles['emojionemonotonewhitecircle']}
+        />
+        <img
+          src="/image26711-i5cg-500w.png"
+          alt="image26711"
+          className={styles['image2']}
+        />
+        <img
+          src="/logoswhatsappicon6711-kwhh.svg"
+          alt="logoswhatsappicon6711"
+          className={styles['logoswhatsappicon']}
+        />
+        <img
+          src="/rectangle406711-884-200h.png"
+          alt="Rectangle406711"
+          className={styles['rectangle40']}
+        />
+        <img
+          src="/icsharparrowbackios6711-hhjo.svg"
+          alt="icsharparrowbackios6711"
+          className={styles['icsharparrowbackios']}
+        />
+        <img
+          src="/materialsymbolscircleoutline6711-a8re.svg"
+          alt="materialsymbolscircleoutline6711"
+          className={styles['materialsymbolscircleoutline']}
+        />
+        <img
+          src="/streamlineinterfacesettingmenu1buttonparallelhoriz6711-fzzp3.svg"
+          alt="streamlineinterfacesettingmenu1buttonparallelhoriz6711"
+          className={
+            styles['streamlineinterfacesettingmenu1buttonparallelhoriz']
+          }
+        />
+        <div className={styles['healthiconsphonenegative']}>
+          <div className={styles['group']}>
+            <div className={styles['clippathgroup']}>
+              <div className={styles['healthicons-phone-negative0']}>
+                <img
+                  src="/vector6711-gs9f.svg"
+                  alt="Vector6711"
+                  className={styles['vector']}
+                />
+              </div>
+              <div className={styles['group1']}>
+                <img
+                  src="/vector6711-f4pm.svg"
+                  alt="Vector6711"
+                  className={styles['vector1']}
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            src="/icsharpphone6711-41s7.svg"
+            alt="icsharpphone6711"
+            className={styles['icsharpphone']}
+          />
+        </div>
+        <img
+          src="/logosspotifyicon6711-96.svg"
+          alt="logosspotifyicon6711"
+          className={styles['logosspotifyicon']}
+        />
+        <div className={styles['group2']}>
+          <img
+            src="/vector6711-u06.svg"
+            alt="Vector6711"
+            className={styles['vector2']}
+          />
+          <img
+            src="/vector6711-u734.svg"
+            alt="Vector6711"
+            className={styles['vector3']}
+          />
+        </div>
+        <img
+          src="/image36711-8h7ly-200h.png"
+          alt="image36711"
+          className={styles['image3']}
+        />
+        <img
+          src="/image214748364821078416711-ngoq-200h.png"
+          alt="IMAGE214748364821078416711"
+          className={styles['image21474836482107841']}
+        />
+        <img
+          src="/image16711-w7m-200h.png"
+          alt="image16711"
+          className={styles['image1']}
+        />
+        <div className={styles['group20']}>
+          <span className={styles['text']}>
+            <span>SOS</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax24
