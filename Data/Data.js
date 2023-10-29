927da5d1670b1aafc74859056141ad73be500d30
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 08:32:58 +0400

    Create Data.js

---
+++ Data/Data.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max13.module.css'
+
+const IPhone1415ProMax13 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max13']}>
+        <img
+          src="/rectangle66069-gax-200h.png"
+          alt="Rectangle66069"
+          className={styles['rectangle6']}
+        />
+        <img
+          src="/rectangle76069-k8nk-200h.png"
+          alt="Rectangle76069"
+          className={styles['rectangle7']}
+        />
+        <img
+          src="/rectangle86069-x46p-200h.png"
+          alt="Rectangle86069"
+          className={styles['rectangle8']}
+        />
+        <img
+          src="/rectangle96069-o3n-200h.png"
+          alt="Rectangle96069"
+          className={styles['rectangle9']}
+        />
+        <span className={styles['text']}>
+          <span>Data</span>
+        </span>
+        <span className={styles['text2']}>
+          <span>
+            <span>There is no Data Available</span>
+            <br></br>
+            <span>at this moment</span>
+          </span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax13
