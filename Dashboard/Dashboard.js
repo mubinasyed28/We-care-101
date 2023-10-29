82f5e5ef80c3e8f2851ddf7b941003d5e425d778
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 07:41:23 +0400

    Add files via upload

---
+++ Dashboard/Dashboard.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max12.module.css'
+
+const IPhone1415ProMax12 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max12']}>
+        <div className={styles['group11']}>
+          <div className={styles['group']}>
+            <img
+              src="/vector5762-058s.svg"
+              alt="Vector5762"
+              className={styles['vector']}
+            />
+            <img
+              src="/vector5762-878j.svg"
+              alt="Vector5762"
+              className={styles['vector1']}
+            />
+          </div>
+          <span className={styles['text']}>
+            <span>Yash Khade</span>
+          </span>
+          <span className={styles['text02']}>
+            <span>+91 9*****</span>
+          </span>
+          <img
+            src="/bxedit5763-pjbo.svg"
+            alt="bxedit5763"
+            className={styles['bxedit']}
+          />
+        </div>
+        <div className={styles['group12']}>
+          <img
+            src="/rectangle525763-0hh-200h.png"
+            alt="Rectangle525763"
+            className={styles['rectangle52']}
+          />
+          <span className={styles['text04']}>
+            <span>Home</span>
+          </span>
+          <img
+            src="/rectangle535764-lkn-200h.png"
+            alt="Rectangle535764"
+            className={styles['rectangle53']}
+          />
+          <span className={styles['text06']}>
+            <span>Connections</span>
+          </span>
+          <img
+            src="/rectangle545764-cwwq-200h.png"
+            alt="Rectangle545764"
+            className={styles['rectangle54']}
+          />
+          <span className={styles['text08']}>
+            <span>Custom Tracking</span>
+          </span>
+          <img
+            src="/rectangle555764-clki-200h.png"
+            alt="Rectangle555764"
+            className={styles['rectangle55']}
+          />
+          <span className={styles['text10']}>
+            <span>Add SOS Widget</span>
+          </span>
+          <img
+            src="/rectangle565764-chi-200h.png"
+            alt="Rectangle565764"
+            className={styles['rectangle56']}
+          />
+          <span className={styles['text12']}>
+            <span>Data</span>
+          </span>
+          <img
+            src="/rectangle575764-v711-200h.png"
+            alt="Rectangle575764"
+            className={styles['rectangle57']}
+          />
+          <span className={styles['text14']}>
+            <span>Settings</span>
+          </span>
+          <img
+            src="/rectangle585765-c5q8-200h.png"
+            alt="Rectangle585765"
+            className={styles['rectangle58']}
+          />
+          <span className={styles['text16']}>
+            <span>Help</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax12
