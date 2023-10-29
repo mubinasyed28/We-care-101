864732c03d447334b42f8f0e06a56a78095e8ffb
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 08:37:36 +0400

    Create settings.js

---
+++ Settings/Settings page/settings.js
@@ -0,0 +1,170 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max10.module.css'
+
+const IPhone1415ProMax10 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max10']}>
+        <img
+          src="/rectangle65045-7tk-200h.png"
+          alt="Rectangle65045"
+          className={styles['rectangle6']}
+        />
+        <div className={styles['group4']}>
+          <img
+            src="/rectangle7i607-az-200h.png"
+            alt="Rectangle7I607"
+            className={styles['rectangle7']}
+          />
+          <img
+            src="/rectangle8i607-6rmn-200h.png"
+            alt="Rectangle8I607"
+            className={styles['rectangle8']}
+          />
+          <img
+            src="/rectangle9i607-xpi4-200h.png"
+            alt="Rectangle9I607"
+            className={styles['rectangle9']}
+          />
+        </div>
+        <span className={styles['text']}>
+          <span>Settings</span>
+        </span>
+        <img
+          src="/rectangle105046-o1x6-200h.png"
+          alt="Rectangle105046"
+          className={styles['rectangle10']}
+        />
+        <span className={styles['text02']}>
+          <span>Edit Profile</span>
+        </span>
+        <img
+          src="/arrow15046-w07bs.svg"
+          alt="Arrow15046"
+          className={styles['arrow1']}
+        />
+        <img
+          src="/rectangle405046-iqw-400h.png"
+          alt="Rectangle405046"
+          className={styles['rectangle40']}
+        />
+        <img
+          src="/rectangle415048-djbt-400w.png"
+          alt="Rectangle415048"
+          className={styles['rectangle41']}
+        />
+        <span className={styles['text04']}>
+          <span>Tracking Permission</span>
+        </span>
+        <span className={styles['text06']}>
+          <span>About Us</span>
+        </span>
+        <img
+          src="/line45046-39oj.svg"
+          alt="Line45046"
+          className={styles['line4']}
+        />
+        <img
+          src="/line55047-bs5c.svg"
+          alt="Line55047"
+          className={styles['line5']}
+        />
+        <img
+          src="/line65047-rjg.svg"
+          alt="Line65047"
+          className={styles['line6']}
+        />
+        <img
+          src="/line75047-xv3.svg"
+          alt="Line75047"
+          className={styles['line7']}
+        />
+        <img
+          src="/line85047-5kb6.svg"
+          alt="Line85047"
+          className={styles['line8']}
+        />
+        <span className={styles['text08']}>
+          <span>Location</span>
+        </span>
+        <span className={styles['text10']}>
+          <span>Microphone</span>
+        </span>
+        <span className={styles['text12']}>
+          <span>Photo</span>
+        </span>
+        <span className={styles['text14']}>
+          <span>Video</span>
+        </span>
+        <span className={styles['text16']}>
+          <span>
+            <span>
+              &quot;Welcome to – your emergency
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <br></br>
+            <span>
+              companion. Stay safe, share your location, and connect with loved
+              ones in real-time. Your safety, our priority.&quot;
+            </span>
+          </span>
+        </span>
+        <div className={styles['component1']}>
+          <img
+            src="/rectangle51i525-e67o-200h.png"
+            alt="Rectangle51I525"
+            className={styles['rectangle51']}
+          />
+          <img
+            src="/ellipse3i525-ibr8-200h.png"
+            alt="Ellipse3I525"
+            className={styles['ellipse3']}
+          />
+        </div>
+        <div className={styles['component2']}>
+          <img
+            src="/rectangle51i535-lwz-200h.png"
+            alt="Rectangle51I535"
+            className={styles['rectangle511']}
+          />
+          <img
+            src="/ellipse3i535-khr6-200h.png"
+            alt="Ellipse3I535"
+            className={styles['ellipse31']}
+          />
+        </div>
+        <div className={styles['component3']}>
+          <img
+            src="/rectangle51i535-g8nk-200h.png"
+            alt="Rectangle51I535"
+            className={styles['rectangle512']}
+          />
+          <img
+            src="/ellipse3i535-i7x9-200h.png"
+            alt="Ellipse3I535"
+            className={styles['ellipse32']}
+          />
+        </div>
+        <div className={styles['component4']}>
+          <img
+            src="/rectangle51i535-p8u-200h.png"
+            alt="Rectangle51I535"
+            className={styles['rectangle513']}
+          />
+          <img
+            src="/ellipse3i535-85sw-200h.png"
+            alt="Ellipse3I535"
+            className={styles['ellipse33']}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax10
