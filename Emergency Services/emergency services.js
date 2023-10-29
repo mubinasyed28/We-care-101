8892a5db65497ba12d349539712408a4a7e20661
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 07:39:53 +0400

    Add files via upload

---
+++ Emergency Services/emergency services.js
@@ -0,0 +1,464 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max4.module.css'
+
+const IPhone1415ProMax4 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max4']}>
+        <img
+          src="/rectangle64235-8q6q-200h.png"
+          alt="Rectangle64235"
+          className={styles['rectangle6']}
+        />
+        <div className={styles['iconparktwotoneback']}>
+          <div className={styles['maskgroup']}>
+            <div className={styles['group']}>
+              <img
+                src="/vectori671-um9.svg"
+                alt="VectorI671"
+                className={styles['vector']}
+              />
+            </div>
+            <img
+              src="/vectori671-d7lg.svg"
+              alt="VectorI671"
+              className={styles['vector01']}
+            />
+          </div>
+        </div>
+        <span className={styles['text']}>
+          <span>CALL FOR HELP</span>
+        </span>
+        <div className={styles['group4']} className={styles['group4']}>
+          <img
+            src="/rectangle104244-ucgn-200h.png"
+            alt="Rectangle104244"
+            className={styles['rectangle10']}
+          />
+          <div className={styles['frameiconemergencyphone']}>
+            <img
+              src="/vector4210-l9oh.svg"
+              alt="Vector4210"
+              className={styles['vector02']}
+            />
+          </div>
+          <img
+            src="/rectangle114247-xuwl-200h.png"
+            alt="Rectangle114247"
+            className={styles['rectangle11']}
+          />
+          <img
+            src="/rectangle124248-l4dd-200h.png"
+            alt="Rectangle124248"
+            className={styles['rectangle12']}
+          />
+          <img
+            src="/rectangle134249-utjs-200h.png"
+            alt="Rectangle134249"
+            className={styles['rectangle13']}
+          />
+          <img
+            src="/rectangle144250-hkur-200h.png"
+            alt="Rectangle144250"
+            className={styles['rectangle14']}
+          />
+          <img
+            src="/rectangle154251-xh1-200h.png"
+            alt="Rectangle154251"
+            className={styles['rectangle15']}
+          />
+          <img
+            src="/rectangle164252-cka3-200h.png"
+            alt="Rectangle164252"
+            className={styles['rectangle16']}
+          />
+          <img
+            src="/rectangle174253-2bx-200h.png"
+            alt="Rectangle174253"
+            className={styles['rectangle17']}
+          />
+          <span className={styles['text02']}>
+            <span className={styles['text03']}>
+              <span>112</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>National Helpline</span>
+          </span>
+          <span className={styles['text08']}>
+            <span className={styles['text09']}>
+              <span>100</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>Police</span>
+          </span>
+          <span className={styles['text14']}>
+            <span className={styles['text15']}>
+              <span>108</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>Ambulance</span>
+          </span>
+          <span className={styles['text20']}>
+            <span className={styles['text21']}>
+              <span>101</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>Fire Service</span>
+          </span>
+          <span className={styles['text26']}>
+            <span className={styles['text27']}>
+              <span>102</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>Pregnancy Medic</span>
+          </span>
+          <span className={styles['text32']}>
+            <span className={styles['text33']}>
+              <span>1073</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>Road Accident</span>
+          </span>
+          <span className={styles['text38']}>
+            <span className={styles['text39']}>
+              <span>1091</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>Women Helpline</span>
+          </span>
+          <span className={styles['text44']}>
+            <span className={styles['text45']}>
+              <span>1098</span>
+              <br></br>
+              <span></span>
+            </span>
+            <span>Child</span>
+          </span>
+          <div className={styles['frameiconphone']}>
+            <img
+              src="/vector4275-diyd.svg"
+              alt="Vector4275"
+              className={styles['vector03']}
+            />
+            <img
+              src="/vector4276-8cxa.svg"
+              alt="Vector4276"
+              className={styles['vector04']}
+            />
+            <img
+              src="/vector4277-avdt.svg"
+              alt="Vector4277"
+              className={styles['vector05']}
+            />
+          </div>
+          <div className={styles['frameiconphone1']}>
+            <img
+              src="/vector4279-s0e.svg"
+              alt="Vector4279"
+              className={styles['vector06']}
+            />
+            <img
+              src="/vector4280-tlju.svg"
+              alt="Vector4280"
+              className={styles['vector07']}
+            />
+            <img
+              src="/vector4281-92hg.svg"
+              alt="Vector4281"
+              className={styles['vector08']}
+            />
+          </div>
+          <div className={styles['frameiconphone2']}>
+            <img
+              src="/vector4283-yfuj.svg"
+              alt="Vector4283"
+              className={styles['vector09']}
+            />
+            <img
+              src="/vector4284-zyl.svg"
+              alt="Vector4284"
+              className={styles['vector10']}
+            />
+            <img
+              src="/vector4285-nfo.svg"
+              alt="Vector4285"
+              className={styles['vector11']}
+            />
+          </div>
+          <div className={styles['frameiconphone3']}>
+            <img
+              src="/vector4287-oxkc.svg"
+              alt="Vector4287"
+              className={styles['vector12']}
+            />
+            <img
+              src="/vector4288-ztnb.svg"
+              alt="Vector4288"
+              className={styles['vector13']}
+            />
+            <img
+              src="/vector4289-gcn.svg"
+              alt="Vector4289"
+              className={styles['vector14']}
+            />
+          </div>
+          <div className={styles['frameiconphone4']}>
+            <img
+              src="/vector4291-o8s.svg"
+              alt="Vector4291"
+              className={styles['vector15']}
+            />
+            <img
+              src="/vector4292-9uqs.svg"
+              alt="Vector4292"
+              className={styles['vector16']}
+            />
+            <img
+              src="/vector4293-di3.svg"
+              alt="Vector4293"
+              className={styles['vector17']}
+            />
+          </div>
+          <div className={styles['frameiconphone5']}>
+            <img
+              src="/vector4295-ngo9.svg"
+              alt="Vector4295"
+              className={styles['vector18']}
+            />
+            <img
+              src="/vector4296-g9f.svg"
+              alt="Vector4296"
+              className={styles['vector19']}
+            />
+            <img
+              src="/vector4297-jkd.svg"
+              alt="Vector4297"
+              className={styles['vector20']}
+            />
+          </div>
+          <div className={styles['frameiconphone6']}>
+            <img
+              src="/vector4299-f2pp.svg"
+              alt="Vector4299"
+              className={styles['vector21']}
+            />
+            <img
+              src="/vector4210-nx0e.svg"
+              alt="Vector4210"
+              className={styles['vector22']}
+            />
+            <img
+              src="/vector4210-rgr.svg"
+              alt="Vector4210"
+              className={styles['vector23']}
+            />
+          </div>
+          <div className={styles['frameiconphone7']}>
+            <img
+              src="/vector4210-t2v.svg"
+              alt="Vector4210"
+              className={styles['vector24']}
+            />
+            <img
+              src="/vector4210-f2b.svg"
+              alt="Vector4210"
+              className={styles['vector25']}
+            />
+            <img
+              src="/vector4210-9xhd.svg"
+              alt="Vector4210"
+              className={styles['vector26']}
+            />
+          </div>
+          <div className={styles['frameicon-mobile-clinic']}>
+            <img
+              src="/vector4210-cqm.svg"
+              alt="Vector4210"
+              className={styles['vector27']}
+            />
+          </div>
+          <div className={styles['frameicon-police-officer']}>
+            <div className={styles['group1']}>
+              <img
+                src="/vector4213-ytwb.svg"
+                alt="Vector4213"
+                className={styles['vector28']}
+              />
+              <img
+                src="/vector4213-80z.svg"
+                alt="Vector4213"
+                className={styles['vector29']}
+              />
+              <img
+                src="/vector4213-4s4u.svg"
+                alt="Vector4213"
+                className={styles['vector30']}
+              />
+              <img
+                src="/vector4213-s98n.svg"
+                alt="Vector4213"
+                className={styles['vector31']}
+              />
+            </div>
+          </div>
+          <div className={styles['frameemojifireengine']}>
+            <img
+              src="/vector4213-f0ln.svg"
+              alt="Vector4213"
+              className={styles['vector32']}
+            />
+            <img
+              src="/vector4213-bp.svg"
+              alt="Vector4213"
+              className={styles['vector33']}
+            />
+            <img
+              src="/vector4213-7t4j.svg"
+              alt="Vector4213"
+              className={styles['vector34']}
+            />
+            <img
+              src="/vector4213-vs7.svg"
+              alt="Vector4213"
+              className={styles['vector35']}
+            />
+            <img
+              src="/vector4213-02s.svg"
+              alt="Vector4213"
+              className={styles['vector36']}
+            />
+            <img
+              src="/vector4214-ae5.svg"
+              alt="Vector4214"
+              className={styles['vector37']}
+            />
+            <img
+              src="/vector4214-35qn.svg"
+              alt="Vector4214"
+              className={styles['vector38']}
+            />
+            <img
+              src="/vector4214-ck99.svg"
+              alt="Vector4214"
+              className={styles['vector39']}
+            />
+            <img
+              src="/vector4214-65fk.svg"
+              alt="Vector4214"
+              className={styles['vector40']}
+            />
+            <img
+              src="/vector4214-2zxr.svg"
+              alt="Vector4214"
+              className={styles['vector41']}
+            />
+            <img
+              src="/vector4214-fyun.svg"
+              alt="Vector4214"
+              className={styles['vector42']}
+            />
+            <img
+              src="/vector4214-sy0o.svg"
+              alt="Vector4214"
+              className={styles['vector43']}
+            />
+            <img
+              src="/vector4214-dhsb.svg"
+              alt="Vector4214"
+              className={styles['vector44']}
+            />
+          </div>
+          <div className={styles['frameicon-pregnant']}>
+            <div className={styles['group2']}>
+              <img
+                src="/vector4215-bzjo.svg"
+                alt="Vector4215"
+                className={styles['vector45']}
+              />
+              <img
+                src="/vector4215-dyvh.svg"
+                alt="Vector4215"
+                className={styles['vector46']}
+              />
+            </div>
+          </div>
+          <div className={styles['group3']}>
+            <img
+              src="/vector4215-zz5.svg"
+              alt="Vector4215"
+              className={styles['vector47']}
+            />
+            <img
+              src="/vector4215-64bi.svg"
+              alt="Vector4215"
+              className={styles['vector48']}
+            />
+            <img
+              src="/vector4215-ubj.svg"
+              alt="Vector4215"
+              className={styles['vector49']}
+            />
+            <img
+              src="/vector4215-er1.svg"
+              alt="Vector4215"
+              className={styles['vector50']}
+            />
+            <div>
+              <img
+                src="/vector4215-hs3z.svg"
+                alt="Vector4215"
+                className={styles['vector51']}
+              />
+              <img
+                src="/vector4215-6ah.svg"
+                alt="Vector4215"
+                className={styles['vector52']}
+              />
+              <img
+                src="/vector4216-gvsd.svg"
+                alt="Vector4216"
+                className={styles['vector53']}
+              />
+            </div>
+          </div>
+          <div className={styles['frameicon-woman']}>
+            <div className={styles['group5']}>
+              <img
+                src="/vector4216-mks8.svg"
+                alt="Vector4216"
+                className={styles['vector54']}
+              />
+              <img
+                src="/vector4216-8dlm.svg"
+                alt="Vector4216"
+                className={styles['vector55']}
+              />
+            </div>
+          </div>
+          <div className={styles['group6']}>
+            <img
+              src="/vector4216-4aj4.svg"
+              alt="Vector4216"
+              className={styles['vector56']}
+            />
+            <img
+              src="/vector4216-ahq.svg"
+              alt="Vector4216"
+              className={styles['vector57']}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax4
