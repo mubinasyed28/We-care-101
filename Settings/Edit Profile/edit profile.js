e09191472bb4bf4306b1f12c54e4d0eae454b867
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 08:39:57 +0400

    Create edit profile.js

---
+++ Settings/Edit Profile/edit profile.js
@@ -0,0 +1,131 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max11.module.css'
+
+const IPhone1415ProMax11 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max11']}>
+        <img
+          src="/rectangle65048-x3h-200h.png"
+          alt="Rectangle65048"
+          className={styles['rectangle6']}
+        />
+        <div className={styles['iconparktwotoneback']}>
+          <div className={styles['maskgroup']}>
+            <div className={styles['group']}>
+              <img
+                src="/vectori671-gtgd.svg"
+                alt="VectorI671"
+                className={styles['vector']}
+              />
+            </div>
+            <img
+              src="/vectori671-ingb.svg"
+              alt="VectorI671"
+              className={styles['vector1']}
+            />
+          </div>
+        </div>
+        <span className={styles['text']}>
+          <span>Settings</span>
+        </span>
+        <div className={styles['frame1']}>
+          <span className={styles['text02']}>
+            <span>Your Profile</span>
+          </span>
+          <img
+            src="/rectangle425150-3m6-500w.png"
+            alt="Rectangle425150"
+            className={styles['rectangle42']}
+          />
+          <span className={styles['text04']}>
+            <span>Personal Details</span>
+          </span>
+          <img
+            src="/rectangle435151-jxvp-500h.png"
+            alt="Rectangle435151"
+            className={styles['rectangle43']}
+          />
+          <img
+            src="/rectangle485152-4jgk-500w.png"
+            alt="Rectangle485152"
+            className={styles['rectangle48']}
+          />
+          <span className={styles['text06']}>
+            <span>Photo</span>
+          </span>
+          <span className={styles['text08']}>
+            <span>Name</span>
+          </span>
+          <div className={styles['ggprofile']}>
+            <div className={styles['group1']}>
+              <img
+                src="/vector5151-nqi8.svg"
+                alt="Vector5151"
+                className={styles['vector2']}
+              />
+              <img
+                src="/vector5151-vhql.svg"
+                alt="Vector5151"
+                className={styles['vector3']}
+              />
+            </div>
+            <img
+              src="/bxedit5151-xhq9.svg"
+              alt="bxedit5151"
+              className={styles['bxedit']}
+            />
+          </div>
+          <img
+            src="/rectangle445152-s2wp-200h.png"
+            alt="Rectangle445152"
+            className={styles['rectangle44']}
+          />
+          <span className={styles['text10']}>
+            <span>Phone Number</span>
+          </span>
+          <img
+            src="/rectangle455152-ce5h-200h.png"
+            alt="Rectangle455152"
+            className={styles['rectangle45']}
+          />
+          <span className={styles['text12']}>
+            <span>Address</span>
+          </span>
+          <img
+            src="/rectangle465152-8g-200h.png"
+            alt="Rectangle465152"
+            className={styles['rectangle46']}
+          />
+          <span className={styles['text14']}>
+            <span>Email</span>
+          </span>
+          <img
+            src="/rectangle475152-6ab7-400w.png"
+            alt="Rectangle475152"
+            className={styles['rectangle47']}
+          />
+          <img
+            src="/rectangle495153-p8p-200h.png"
+            alt="Rectangle495153"
+            className={styles['rectangle49']}
+          />
+          <span className={styles['text16']}>
+            <span>Cancel</span>
+          </span>
+          <img
+            src="/rectangle505153-yznn-200h.png"
+            alt="Rectangle505153"
+            className={styles['rectangle50']}
+          />
+          <span className={styles['text18']}>
+            <span>Save</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax11
