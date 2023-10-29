9b175ae618f74d54e8dc87c6dace15567abd7c9e
Mubina <146509908+mubinasyed28@users.noreply.github.com>
2023-10-29 07:43:16 +0400

    Add files via upload

---
+++ Add connections/Add From Contacts/add connection from contacts.js
@@ -0,0 +1,205 @@
+import React from 'react'
+
+import styles from './i-phone1415-pro-max6.module.css'
+
+const IPhone1415ProMax6 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['i-phone1415-pro-max6']}>
+        <img
+          src="/rectangle64230-octl-200h.png"
+          alt="Rectangle64230"
+          className={styles['rectangle6']}
+        />
+        <span className={styles['text']}>
+          <span>CONNECTIONS</span>
+        </span>
+        <div className={styles['group8']}>
+          <span className={styles['text02']}>
+            <span>ADD FROM CONTACTS</span>
+          </span>
+          <img
+            src="/rectangle184230-r3if-200h.png"
+            alt="Rectangle184230"
+            className={styles['rectangle18']}
+          />
+          <img
+            src="/rectangle234230-7xh9-200h.png"
+            alt="Rectangle234230"
+            className={styles['rectangle23']}
+          />
+          <img
+            src="/rectangle244230-vs0a-200h.png"
+            alt="Rectangle244230"
+            className={styles['rectangle24']}
+          />
+          <img
+            src="/rectangle254332-k79d-200h.png"
+            alt="Rectangle254332"
+            className={styles['rectangle25']}
+          />
+          <img
+            src="/rectangle264332-cqgl-200h.png"
+            alt="Rectangle264332"
+            className={styles['rectangle26']}
+          />
+          <img
+            src="/rectangle274332-xzix-200h.png"
+            alt="Rectangle274332"
+            className={styles['rectangle27']}
+          />
+          <img
+            src="/rectangle284332-lv48-200h.png"
+            alt="Rectangle284332"
+            className={styles['rectangle28']}
+          />
+          <img
+            src="/rectangle294333-j6l-200h.png"
+            alt="Rectangle294333"
+            className={styles['rectangle29']}
+          />
+          <img
+            src="/rectangle304333-ub6l-200h.png"
+            alt="Rectangle304333"
+            className={styles['rectangle30']}
+          />
+          <img
+            src="/rectangle314333-ruv-200h.png"
+            alt="Rectangle314333"
+            className={styles['rectangle31']}
+          />
+          <img
+            src="/rectangle324333-e8m5-200h.png"
+            alt="Rectangle324333"
+            className={styles['rectangle32']}
+          />
+          <span className={styles['text04']}>
+            <span>Yash Khade</span>
+          </span>
+          <span className={styles['text06']}>
+            <span>Mubina Syed</span>
+          </span>
+          <span className={styles['text08']}>
+            <span>Sheshank Singh</span>
+          </span>
+          <span className={styles['text10']}>
+            <span>Contact Name</span>
+          </span>
+          <span className={styles['text12']}>
+            <span>Contact Name</span>
+          </span>
+          <span className={styles['text14']}>
+            <span>Contact Name</span>
+          </span>
+          <span className={styles['text16']}>
+            <span>Contact Name</span>
+          </span>
+          <span className={styles['text18']}>
+            <span>Contact Name</span>
+          </span>
+          <span className={styles['text20']}>
+            <span>Contact Name</span>
+          </span>
+          <span className={styles['text22']}>
+            <span>Contact Name</span>
+          </span>
+          <div className={styles['mdicheckboxblankcircleoutline']}>
+            <img
+              src="/richeckboxcirclefilli463-irwn.svg"
+              alt="richeckboxcirclefillI463"
+              className={styles['richeckboxcirclefill']}
+            />
+          </div>
+          <img
+            src="/mdicheckboxblankcircleoutline4636-2xyr.svg"
+            alt="mdicheckboxblankcircleoutline4636"
+            className={styles['mdicheckboxblankcircleoutline01']}
+          />
+          <img
+            src="/mdicheckboxblankcircleoutline4636-tsgs.svg"
+            alt="mdicheckboxblankcircleoutline4636"
+            className={styles['mdicheckboxblankcircleoutline02']}
+          />
+          <img
+            src="/mdicheckboxblankcircleoutline4636-acjf.svg"
+            alt="mdicheckboxblankcircleoutline4636"
+            className={styles['mdicheckboxblankcircleoutline03']}
+          />
+          <div className={styles['mdicheckboxblankcircleoutline04']}>
+            <img
+              src="/richeckboxcirclefilli463-2n6g.svg"
+              alt="richeckboxcirclefillI463"
+              className={styles['richeckboxcirclefill1']}
+            />
+          </div>
+          <img
+            src="/mdicheckboxblankcircleoutline4637-67kk.svg"
+            alt="mdicheckboxblankcircleoutline4637"
+            className={styles['mdicheckboxblankcircleoutline05']}
+          />
+          <img
+            src="/mdicheckboxblankcircleoutline4637-86im.svg"
+            alt="mdicheckboxblankcircleoutline4637"
+            className={styles['mdicheckboxblankcircleoutline06']}
+          />
+          <img
+            src="/mdicheckboxblankcircleoutline4637-xcs.svg"
+            alt="mdicheckboxblankcircleoutline4637"
+            className={styles['mdicheckboxblankcircleoutline07']}
+          />
+          <img
+            src="/mdicheckboxblankcircleoutline4637-fe4.svg"
+            alt="mdicheckboxblankcircleoutline4637"
+            className={styles['mdicheckboxblankcircleoutline08']}
+          />
+          <img
+            src="/mdicheckboxblankcircleoutline4638-fpc6.svg"
+            alt="mdicheckboxblankcircleoutline4638"
+            className={styles['mdicheckboxblankcircleoutline09']}
+          />
+        </div>
+        <div className={styles['group7']}>
+          <img
+            src="/line34333-8ksk.svg"
+            alt="Line34333"
+            className={styles['line3']}
+          />
+          <img
+            src="/rectangle354333-rk2o-200h.png"
+            alt="Rectangle354333"
+            className={styles['rectangle35']}
+          />
+          <img
+            src="/rectangle364333-mlde-200h.png"
+            alt="Rectangle364333"
+            className={styles['rectangle36']}
+          />
+          <span className={styles['text24']}>
+            <span>CANCEL</span>
+          </span>
+          <span className={styles['text26']}>
+            <span>ADD</span>
+          </span>
+        </div>
+        <div className={styles['iconparktwotoneback']}>
+          <div className={styles['maskgroup']}>
+            <div className={styles['group']}>
+              <img
+                src="/vectori671-owbo.svg"
+                alt="VectorI671"
+                className={styles['vector']}
+              />
+            </div>
+            <img
+              src="/vectori671-wwj.svg"
+              alt="VectorI671"
+              className={styles['vector1']}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IPhone1415ProMax6
